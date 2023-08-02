996c28383a670e0b77ba734e97d4c6f166ef382e
sedrik13 <122992058+sedrik13@users.noreply.github.com>
2023-08-02 12:58:58 +0300

    Update BonusServiceTest.java

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -28,7 +28,7 @@ class BonusServiceTest {
     void shouldCalculateForNotRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
         long amount = 1000;
-        boolean registered = true;
+        boolean registered = false;
         long expected = 10;
         long actual = service.calculate(amount, false);
         Assertions.assertEquals(expected, actual);
@@ -38,7 +38,7 @@ class BonusServiceTest {
     void shouldCalculateForNotRegisteredAndOverLimit() {
         BonusService service = new BonusService();
         long amount = 1_000_000;
-        boolean registered = true;
+        boolean registered = false;
         long expected = 500;
         long actual = service.calculate(amount, false);
         Assertions.assertEquals(expected, actual);
